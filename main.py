55f73abdc29fe01a8ad8087786cb15b2f06d80ae
bdk1210 <bdk1210@icloud.com>
2025-07-26 16:57:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ statuses = [
     (discord.Status.dnd, discord.Activity(type=discord.ActivityType.listening, name="the screams of the damned :3")),
     (discord.Status.idle, discord.Activity(type=discord.ActivityType.watching, name="you sleep")),
     (discord.Status.online, discord.Activity(type=discord.ActivityType.competing, name="the hunger games"))
-    (discord.Status.streaming, discord.Streaming(name="the one and only", url="https://twitch.tv/lofigirl")),
+    (discord.Status.online, discord.Streaming(name="the one and only", url="https://twitch.tv/lofigirl")),
 ]
 
 status_cycle = itertools.cycle(statuses) # idk what this does but chatgpt said it "create the cycle iterator once"
