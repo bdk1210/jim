1b8347e19fbf0c98a287cc5c2a01c27968fa058a
bdk1210 <bdk1210@icloud.com>
2025-08-02 14:33:17 -0400

    fixed syntax error in greetings.py

--- cogs/greetings.py
+++ cogs/greetings.py
@@ -25,34 +25,34 @@ class Greetings(commands.Cog):
             random_number = random.randint(60, 100)
             # Send the message
             await message.channel.send(f"hi\n-# {random_number}% confidence")
-            ]
+            return
 
-     # how to join feature
-    # List of trigger phrases (lowercase)
-    htj_triggers = [
-        "how to join",
-        "how do i join",
-        "how can i join",
-        "join instructions",
-        "how do you join",
-        "htj",
-    ]
-
-    ily_triggers = [
-        "i love you jim",
-        "jim i love you",
-        "i love jim"]
-    
-    ily_responses = [
-        "i can't say it back but i appreciate the sentiment! :3",
-        "thanks but i'm not saying it back",
-        "i know what you're trying to do, and i won't say it back",
-        "thank you but you aren't fooling me."
+        # how to join feature
+        # List of trigger phrases (lowercase)
+        htj_triggers = [
+            "how to join",
+            "how do i join",
+            "how can i join",
+            "join instructions",
+            "how do you join",
+            "htj",
         ]
-    
-    if any(phrase in message.content.lower() for phrase in ily_triggers):
-        ilymessage = random.choice(ily_responses)
-        await message.channel.send(ilymessage)
+
+        ily_triggers = [
+            "i love you jim",
+            "jim i love you",
+            "i love jim"]
+        
+        ily_responses = [
+            "i can't say it back but i appreciate the sentiment! :3",
+            "thanks but i'm not saying it back",
+            "i know what you're trying to do, and i won't say it back",
+            "thank you but you aren't fooling me."
+            ]
+        
+        if any(phrase in message.content.lower() for phrase in ily_triggers):
+            ilymessage = random.choice(ily_responses)
+            await message.channel.send(ilymessage)
             
 
 async def setup(bot):
